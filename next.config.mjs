d3c60454ea847f6e3ed22da2be95211a3b742429
TalitoKate <tarlykova021105@gmail.com>
2025-12-04 22:24:03 +0100

    Remove incorrect redirect rule that was causing 404 on downloads

--- next.config.mjs
+++ next.config.mjs
@@ -8,15 +8,6 @@ const nextConfig = {
       },
     ],
   },
-  async redirects() {
-    return [
-      {
-        source: "/products/download/:downloadVerificationId",
-        destination: "/api/download/:downloadVerificationId",
-        permanent: false,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
